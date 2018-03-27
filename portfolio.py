9c39740e33c031884eec4693639a030f50d564d6
pbeiter <philipp.beiter@nrel.gov>
2018-03-26 18:55:33 -0600

    Extended portfolio classes

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import openpyxl as pyxl
 
 class _Portfolio (object):
 	def __init__(self, indata):
@@ -16,49 +17,54 @@ class analysis(_Portfolio):
 	def summary_stats(self, column):
 		
 		self.data = self.data[column]
-
 		self.mean = self.data.mean()
 		self.min = self.data.min()
 		self.max = self.data.max()
 		self.std = self.data.std()
 
-		#for placeholder in ['mean', 'min', 'max', 'std']:
-		#	self.placeholder = np.round(placeholder, decimals = 2)
+		#self.bv = self.data['Purchase amount'] * self.data['Purchase price'] # buy value
+		#self.pv = (self.data['Purchase amount'] - self.data['Sell amount']) * self.data['Market price'] #present value
 
 		return self.mean, self.min, self.max, self.std
 
-### Portfolio p1
-data1 = pd.DataFrame({'row1': [25, 30, 45],
-					  'row2': [50, 34, 98],
-					  'row3': [100, 56, 80],
-					})
+### Data read-in
+wb = pyxl.load_workbook('portfolio.xlsx')
 
-data2 = pd.DataFrame({'row1': [25, 30, 45],
-					  'row2': [50, 34, 98],
-					  'row3': [9000, 3500, 500000],
-					})
+data = {}
+portfolio_set = {}
 
-#infile = XXX
-#for count, p in enumerate(portfolio, 0):
-#	portfolio[count] = pd.read
-#portfolio_set = pd.read_csv(infile, sheet_name = 'X', header=0)
-portfolio_set = {'Portfolio1': data1, 
-				 'Portfolio2': data2,
-				 }
+for count, sheet in enumerate(wb.worksheets, 0):
+	#name = 'p{}'.format(count)
+	data[count] = pd.DataFrame(sheet.values)
+	columns = data[count].iloc[0]
 
+	data[count].columns = columns
+	data[count] = data[count][1:]
+
+	portfolio_set[count] = data[count]
+
+'''
+#/ Create dict of portfolio: data
+portfolio_set = {}
+for count, d in enumerate(data, 0):
+	portfolio_set[d] = data[count]
+'''
+
+#/ Create portfolio objects
 if __name__ == '__main__':
     portfolios = []
-    #for obj in range(len(portfolio_set)):
     for obj in portfolio_set:
         obj = analysis(portfolio_set[obj])
         portfolios.append(obj)
 
 out = {}
 for count, n in enumerate(portfolio_set,0):
-	out[n] = portfolios[count].summary_stats('row3')
+	out[n] = portfolios[count].summary_stats('Sell price')
 
 out_dat = pd.DataFrame.from_dict(out, orient='columns') #orient='index' for column-row inverse
 out_dat = out_dat.set_axis(['mean', 'min', 'max', 'std'], axis=0, inplace=False) #axis = 1 for column-row inverse
 
 out_dat['mean'] = out_dat.mean(axis=1)
+
+#np.round(placeholder, decimals = 2)
 
